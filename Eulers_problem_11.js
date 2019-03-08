6b855798435993c7cb18ba292d3afa293aea1331
Haftamu Kahsay <kaftamu.skahsay@gmail.com>
2019-03-08 12:14:40 +0300

    i have modified my Euler problem no 11

--- Eulers_problem_11.js
+++ Eulers_problem_11.js
@@ -1,7 +1,7 @@
 // Haftamu Kahsay
 // 24/02/2019
 // Find the prodact of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
-
+/*
 let data = [08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08,
     49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00,
     81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65,
@@ -25,5 +25,6 @@ let data = [08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 
 
     let maxDiagonal = maxVertical= maxHorizontal = 0;
 
-    for(let i=0; i<j; )
+    for(let i=0; i<j; )*/
 
+    console.log("hey you how is you.... am good");
